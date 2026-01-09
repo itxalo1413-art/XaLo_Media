b02d8ac846f1fea68ca328b23fbd3d005ed4150d
hungtan1406 <hungtanth1406@gmail.com>
2026-01-09 17:32:13 +0700

    fix connect local FE

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,18 @@ import { connectDB } from "./config/db";
 import { createApp } from "./app";
 import { seedAdmin } from "./features/auth/auth.service";
 import { swaggerSpec } from "./config/swagger";
+import { requireAdmin } from "./middlewares/auth";
 
 async function bootstrap() {
   await connectDB();
   await seedAdmin();
 
   const app = createApp();
-  app.get("/api/v1/docs-json", (req, res) => res.json(swaggerSpec));
-
+  app.get('/api/v1/admin', requireAdmin, (req, res) => {
+  res.json({
+    message: 'Chào mừng bạn đến với trang quản trị!',
+  });
+});
   app.listen(env.PORT, () => console.log(`🚀 Server running on :${env.PORT}`));
 }
 
